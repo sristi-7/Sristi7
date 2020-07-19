bcc1f8e0c2c517b8385f9d5984095786de4f98c9
sristi <send2sristi@gmail.com>
2020-07-19 17:04:18 +0530

    redball

--- sketch.js
+++ sketch.js
@@ -1,9 +1,12 @@
 var ball;
-
+var database, position;
 function setup(){
     createCanvas(500,500);
+    database= firebase.database();
     ball = createSprite(250,250,10,10);
     ball.shapeColor = "red";
+    var ballPositionref=database.ref("ball/position");
+    ballPositionref.on("value",readPos,showError);
 }
 
 function draw(){
@@ -24,6 +27,19 @@ function draw(){
 }
 
 function changePosition(x,y){
-    ball.x = ball.x + x;
-    ball.y = ball.y + y;
+    database.ref("ball/position").set({
+        x : ball.x + x,
+        y : ball.y + y
+    })
+
+   
+}
+function showError (){
+    console.log("errors");
+}
+function readPos (data){
+    position=data.val();
+    ball.x=position.x;
+    ball.y=position.y;
+
 }
