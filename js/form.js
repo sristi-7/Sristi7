df058b2cea49ae9f161c6602f3497e43d7782cca
sristi <send2sristi@gmail.com>
2020-08-02 17:01:18 +0530

    c35

---
+++ js/form.js
@@ -0,0 +1,22 @@
+class Form {
+    constructor (){
+
+    }
+    display(){
+        var title=createElement("h2");
+        title.html("car Racing Game")
+        title.position(130,0);
+        var input=createInput("name");
+        var button=createButton("play");
+        input.position(130,160);
+        button.position(250,200);
+        button.mousePressed(function(){
+            input.hide();
+            button.hide();
+
+            var name=input.value();
+            playerCount++;
+            
+        })
+    }
+}
